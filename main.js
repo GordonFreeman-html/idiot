a6d8cf02b2f7f75fab75d67ac32256223100b0d4
GordonFreeman-html <v.jones3@share.epsb.ca>
2025-03-21 13:38:56 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+//program title
+console.log("hi");
+document.getElementById("bless").addEventListener("click",answer1);
+document.getElementById("dime").addEventListener("click",answer2);
+document.getElementById("late").addEventListener("click",answer3);
+document.getElementById("pain").addEventListener("click",answer4);
+document.getElementById("flies").addEventListener("click",answer5);
+
+function answer1(){
+let answer1 = alert("feeds you");
+   alert(msg) ;
+}
+function answer2(){
+    let answer2 = alert("bottle");
+       alert(msg) ;
+    }
+    function answer3(){
+     let answer3 = alert("shove");
+           alert(msg) ;
+        }function answer4(){
+    let answer4 = alert("cold");
+               alert(msg) ;
+            }function answer5(){
+let answer5 = alert("over");
+                   alert(msg) ;
+                }
+
+
+
+
